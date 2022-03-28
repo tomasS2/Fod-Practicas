43cfc41f4f19a7bb41707515ffa05b01512f8446
Tomas <101843390+tomasS2@users.noreply.github.com>
2022-03-27 22:25:56 -0300

    Update pra2ejer3fod.pas

--- Practica 2/pra2ejer3fod.pas
+++ Practica 2/pra2ejer3fod.pas
@@ -17,6 +17,8 @@ un mismo detalle cada producto aparece a lo sumo una vez.
 }
 
 program p2ej3fod;
+const	
+	VALOR_ALTO=9999;
 type
 	cad30=string[30];
 	rProducto=record
@@ -28,11 +30,88 @@ type
 	end;
 	
 	rPedido=record
+		sucursalQuePide:integer;
 		codProd:integer;
 		cantPed:integer;
+	end;
+	
+	archProd= file of rProducto;//mae
+	archCarga = file of rPedido;//det
+	
+	aArchCarga= array [1..4]of archCarga;
+	aPedidos= array [1..4]of rPedido;
+	
+procedure cargarArchivoMaestro (var mae:archProd; var aDet:aArchCarga);
+	procedure leerDetalle (var det:archCarga; var rPed:rPedido);
+	begin
+		if not eof (det) then 
+			read(det,rPed)
+		else
+			rPed.codProd:=VALOR_ALTO;
+	end;
+	
+	procedure minimo (var aPedi:aPedidos; var min: rPedido; var aDet:aArchCarga);
+	var
+		minCod,i,indiMin:integer;
+	begin
+		minCod:=9999;
+		for i:=1 to 4 do begin
+			if (aPedi[i].codProd<minCod)then begin
+				minCod:=aPedi[i].codProd;
+				indiMin:=i;
+			end
+		end;
+		min:=aPedi[indiMin];
+		leerDetalle(aDet[indiMin],aPedi[indiMin]);
+	end;
+	
+var
+	informeProdDebajoDeStock:text;
+	informeNoSatisfacen:text;
+	min:rPedido;
+	aPedi:aPedidos;
+	i,falta:integer;
+	regm:rProducto;
+begin
+	assign(informeProdDebajoDeStock,'debajoDeStockMinimo.txt');
+	rewrite(informeProdDebajoDeStock);
+	assign(informeNoSatisfacen,'prodNoSatisfacen.txt');
+	rewrite(informeNoSatisfacen);
+	assign(mae,'archMaestroEj3Pr2');
+	reset(mae);
+	for i:=1 to 4 do begin
+		assign(aDet[i],'archDetalleEj3pr2_'+Chr(i+48));
+		reset(aDet[i]);
+		leerDetalle(aDet[i],aPedi[i]);
+	end;
+	minimo(aPedi,min,aDet);
+	while (min.codProd<>VALOR_ALTO) do begin
+		read(mae,regm);
+		while (regm.codProd<>min.codProd) do 
+			read(mae,regm);
+		while (regm.codProd=min.codProd) do begin 
+			regm.stockActual:=regm.stockActual-min.cantPed;
+			if (regm.stockActual-min.cantPed<0)then//b 
+				falta:=min.cantPed-regm.stockActual;
+				write(informeNoSatisfacen,falta,regm.codProd,min.sucursalQuePide);
+			minimo(aPedi,min,aDet);
+		end;
+		if (regm.stockActual<regm.stockMinimo)then//a
+			write(informeProdDebajoDeStock,regm.codProd);
+	end;
+	close (mae);
+	close (informeProdDebajoDeStock);
+	close (informeNoSatisfacen);
+	for i:=1 to 4 do 
+		close(aDet[i]);
+end;
 	
 	
-	archProd= file of rProducto;
-	archCarga = file of rPedido;
+var
+	mae:archProd;
+	aDet:aArchCarga;
+begin
+	cargarArchivoMaestro(mae,aDet);
+end.
 	
 	
